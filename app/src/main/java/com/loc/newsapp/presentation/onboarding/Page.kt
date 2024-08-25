2a15e874e50113eaa29ef4558bd72525f2ac89e1
Dotingo <m9132824@gmail.com>
2024-08-25 16:15:49 +0300

    newsApp created

---
+++ app/src/main/java/com/loc/newsapp/presentation/onboarding/Page.kt
@@ -0,0 +1,28 @@
+package com.loc.newsapp.presentation.onboarding
+
+import androidx.annotation.DrawableRes
+import com.loc.newsapp.R
+
+data class Page(
+    val title: String,
+    val description: String,
+    @DrawableRes val image: Int
+)
+
+val pages = listOf(
+    Page(
+        title = "Смотрите свежие новости",
+        description = "Смотрите свежие новости на главном экране",
+        image = R.drawable.img1
+    ),
+    Page(
+        title = "Ищите нужные новости",
+        description = "Ищите интересующие вас новости на экране поиска.",
+        image = R.drawable.img2
+    ),
+    Page(
+        title = "Сохраняйте новости в закладки",
+        description = "Сохраняйте новости необходимые новости в закладки и смотрите их позже на экране закладок",
+        image = R.drawable.img3
+    )
+)
---
+++ app/src/main/java/com/loc/newsapp/util/Constants.kt
@@ -0,0 +1,14 @@
+package com.loc.newsapp.util
+
+object Constants {
+
+    const val USER_SETTINGS = "userSettings"
+
+    const val APP_ENTRY = "appEntry"
+
+    const val API_KEY = "9b534190592d43a29c8969275e43bfd4"
+
+    const val BASE_URL = "https://newsapi.org/v2/"
+
+    const val NEWS_DATABASE_NAME = "news_db"
+}
